ff17004ab6d705b9b7629343c746657c65eabc3c
Picocu <founderoffounders@gmail.com>
2020-05-02 17:36:05 +0500

    I made index page with navbar where we can start backend

---
+++ my_restaurant/resources/views/inc/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+        <a class="navbar-brand" href="#">Restaurant</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#service">Menu</a>
+                </li>  
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Reservation</a>
+                </li>  
+                <li class="nav-item">
+                    <a class="nav-link" href="#menu">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Login</a>
+                </li> 
+            </ul>
+        </div>  
+    </nav>
---
+++ my_restaurant/resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+    	<h1>Welcome To Restaurant!</h1>
+    	<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem beatae iste eum laboriosam, illo dolorum ea reiciendis, provident, voluptas vero facilis illum optio quae. Hic voluptatibus, iste provident voluptas laboriosam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste est aspernatur asperiores cum laboriosam doloribus illo eos pariatur, ipsum vero autem voluptates, cumque debitis perferendis, possimus velit. Vitae, corrupti, aspernatur. </p>
+    
+
+    </div>         
+@endsection
