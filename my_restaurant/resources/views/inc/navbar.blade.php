0645e559c792e662089f7d14bdee947f422d1169
Picocu <founderoffounders@gmail.com>
2020-05-03 14:31:07 +0500

    just a little change

--- my_restaurant/resources/views/inc/navbar.blade.php
+++ my_restaurant/resources/views/inc/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-        <a class="navbar-brand" href="#">Restaurant</a>
+        <a class="navbar-brand" href="{{ route('getHome') }}">Restaurant</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
