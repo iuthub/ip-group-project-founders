17304420746d17f06515141b53b41c5ebfd7357a
Fazliddin-8 <fazliddin.ahmedov@onbox.ru>
2020-05-03 19:43:16 +0500

    fixed registration bug

--- my_restaurant/database/seeds/DatabaseSeeder.php
+++ my_restaurant/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ReservationTableSeeder::class);
-    	$this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
