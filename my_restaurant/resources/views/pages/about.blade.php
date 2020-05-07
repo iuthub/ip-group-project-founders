79d0df3ed2079a2369667501d9edabc9bcfb5df6
Alpha212 <anvarjone.kuziboev@gamil.com>
2020-05-08 00:11:59 +0500

    about page changed

--- my_restaurant/resources/views/pages/about.blade.php
+++ my_restaurant/resources/views/pages/about.blade.php
@@ -8,11 +8,9 @@
 			<div class="row align-items-lg-center">
 				<div class="col-12 col-md-6 text-center text-md-left">
 					<div class="section-heading mb-3">
-						<h4>Discover</h4>
 						<h1 class="display-4">Our Story</h1>
 					</div>
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-					<a class="btn mt-4 mb-5 mb-md-0" href="https://girlandthegoat.com">Find More</a>
 				</div>
 				<div class="col-12 col-md-6">
 					<img class="img-fluid" src="{{ URL::to('images/res_about.jpg') }}" width="800">
@@ -23,7 +21,7 @@
 	</section>
 </main>
 <section class="yep">
-<div class="container-fluid ">
+<div class="container">
 <h1 class="display">Our Team</h1>
 </div>
 </section>
@@ -33,7 +31,7 @@
 			<div class="card">
 				<div class="card-body">
 					
-					<img  class="img-fluid rounded-circle w-50 mb-3" src="{{ URL::to('images/cooker1.jpg') }}" >
+					<img  class="img-fluid rounded-circle w-50 mb-3 forker" src="{{ URL::to('images/cooker1.jpg') }}" >
 					<h4 class="fazlidd-TITLE">
 						Jack Daniels
 					</h4>
@@ -62,7 +60,7 @@
 			<div class="card">
 				<div class="card-body">
 					
-					<img  class="img-fluid rounded-circle w-50 mb-3" src="{{ URL::to('images/cooker2.jpg') }}"  >
+					<img  class="img-fluid rounded-circle w-50 mb-3 forker" src="{{ URL::to('images/cooker2.jpg') }}"  >
 					<h4 class="fazlidd-TITLE">
 					Steve Jackson	
 					</h4>
@@ -87,7 +85,7 @@
 			<div class="card">
 				<div class="card-body">
 			
-					<img class="img-fluid rounded-circle w-50 mb-3" src="{{ URL::to('images/cooker3.jpg') }}"   >
+					<img class="img-fluid rounded-circle w-50 mb-3 forker" src="{{ URL::to('images/cooker3.jpg') }}"   >
 					<h4 class="fazlidd-TITLE">
 					Riley Reid
 					</h4>
