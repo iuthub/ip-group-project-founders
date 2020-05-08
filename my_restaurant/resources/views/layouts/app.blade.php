c97ab5f66e4e3235a8f067ebea3375b75ee6c041
Picocu <founderoffounders@gmail.com>
2020-05-08 15:37:09 +0500

    custom favicon is added, yay!

--- my_restaurant/resources/views/layouts/app.blade.php
+++ my_restaurant/resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
         <link href="{{ URL::to('css/styles.css') }}" rel="stylesheet">
+        <link rel="icon" href="{{ URL::to('images/favicon.png') }}" sizes="5x5">
         <title>{{ 'Déjà vu'}}
         </title>
     </head>
