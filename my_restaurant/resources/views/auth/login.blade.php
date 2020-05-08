6c34095d61392eeb2397dae98701a2403561e1f2
Fazliddin-8 <fazliddin.ahmedov@inbox.ru>
2020-05-08 22:22:18 +0500

    fixed bugs in the registration page

--- my_restaurant/resources/views/auth/login.blade.php
+++ my_restaurant/resources/views/auth/login.blade.php
@@ -11,17 +11,17 @@
             <div class="form-group">
                <input   placeholder="Email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                @error('email')
-               <span class="invalid-feedback" role="alert">
-               <strong>{{ $message }}</strong>
-               </span>
+                  <span class="invalid-feedback" role="alert">
+                     <strong>{{ $message }}</strong>
+                  </span>
                @enderror
             </div>
             <div class="form-group">
                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
                @error('password')
-               <span class="invalid-feedback" role="alert">
-               <strong>{{ $message }}</strong>
-               </span>
+                  <span class="invalid-feedback" role="alert">
+                     <strong>{{ $message }}</strong>
+                  </span>
                @enderror
             </div>
             <button type="submit" class="btn btn-success">Submit</button>
@@ -30,8 +30,5 @@
       <div class="col-lg-4 col-md-3 col-sm-2 col-xs-2"></div>
    </div>
 </div>
-
-
-
 
 @endsection
--- my_restaurant/resources/views/auth/register.blade.php
+++ my_restaurant/resources/views/auth/register.blade.php
@@ -9,33 +9,33 @@
                  @csrf
                 <h1>Register</h1>
                 <div class="form-group">
-                    <input  class="form-control"  placeholder="Name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus/>
+                    <input placeholder="Name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus/>                    
                     @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                     @enderror
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
                 <div class="form-group">
-                    <input  class="form-control"  placeholder="Email"id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" />
+                    <input placeholder="Email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                    
                      @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
                      @enderror
-
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control"  placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" />
+                    <input type="password" placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" />
+                    
                     @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
                      @enderror
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Confirm Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" />
-
+                    <input type="password" placeholder="Confirm Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" />
                 </div>
                 <button type="submit" class="btn btn-success">Register</button>
             </form>
