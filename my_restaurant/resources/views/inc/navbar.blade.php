3e6715d8884856419c81b9a70f8bf23bb8e248f4
Picocu <founderoffounders@gmail.com>
2020-05-08 10:12:45 +0500

    Designed NAV, made it responsive, added logo, gave name to restaurant, added jquery function, google font is used and so on

--- my_restaurant/resources/views/inc/navbar.blade.php
+++ my_restaurant/resources/views/inc/navbar.blade.php
@@ -1,11 +1,22 @@
-<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-        <a class="navbar-brand" href="{{ route('getHome') }}">Restaurant</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top " >
+
+
+    
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    
+    <div class="collapse navbar-collapse"  id="collapsibleNavbar" >
+
+    <div class="navbar-collapse  w-100 order-1 " >
+        <a class="navbar-brand mr-auto " href="{{ route('getHome') }}"><img src="{{ URL::to('images/logo.png') }}" class="logo"></a>
+    </div>
+
+
+    <div class="navbar-collapse  w-100 order-2 "  >
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item ">
                     <a class="nav-link" href="{{ route('getHome') }}">Home</a>
                 </li>
                 <li class="nav-item">
@@ -14,26 +25,34 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route((Auth::check() && Auth::user()->is_admin)?'getAdminReservation':'getReservation') }}">Reservation</a>
                 </li>  
-                <li class="nav-item">
+                <li class="nav-item ">
                     <a class="nav-link" href="{{ route('getAbout') }}">About</a>
                 </li>
+            </ul>
+
+    </div>
+
+    <div class="navbar-collapse w-100 order-3"  >
+            <ul class="navbar-nav ml-auto">
 
                 @if(!Auth::check())
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    <li class="nav-item " >
+                        <a class="nav-link sign_in" href="{{ route('login') }}">Login</a>
                     </li> 
+                    <li class="nav-item ">
+                        <a class="nav-link sign_up" href="{{ route('register') }}">Sign Up</a>
+                    </li>
                 @else
-                    <form method="POST" action="{{ route('logout') }}">
+                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <li class="nav-item"> 
-                             <input type="submit" value="Logout" class=""> 
+                        <li class="nav-item "> 
+                             <input type="submit" value="Logout" class = "log_out"> 
                         </li>
                     </form>
+
                 @endif
             </ul>
-        </div>  
-    </nav>
+    </div> 
+</div>
+</nav>
     
