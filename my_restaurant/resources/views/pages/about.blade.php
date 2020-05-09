1b2aabfdfb70a4852b0fd538db4ee5d83cfb1493
Picocu <founderoffounders@gmail.com>
2020-05-09 19:21:44 +0500

    Staff section is improved, new features are added, about page is ready to go

--- my_restaurant/resources/views/pages/about.blade.php
+++ my_restaurant/resources/views/pages/about.blade.php
@@ -24,7 +24,9 @@
                     <div class="col-md-6 col-sm-6  text-center ">
                         <div >
                             <h3 id="para1">What We Do.</h3>
-                            
+                        </div>
+                        <div>
+                        	<img src="{{ URL::to('images/line.png') }}">
                         </div>
                     
                         <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In fugit praesentium maiores eos omnis. Provident magnam dolore, perspiciatis nesciunt quisquam illo, distinctio ullam accusamus id reiciendis. Doloremque dolore recusandae aut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam possimus vitae dolores id voluptates nemo tempore porro veritatis ullam deleniti quia, omnis sequi eum officia expedita commodi, suscipit nostrum minima.</p>
@@ -58,8 +60,12 @@
                     </div>
                    
                     <div class="col-md-6 col-sm-6  text-center ">
-                        <div class=>
+                        <div>
                             <h3 id="para1">How we do</h3>
+                        </div>
+                         <div>
+                        	<img src="{{ URL::to('images/line.png') }}">
+                        </div>
                    
 
                         <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ea, dolorem consequatur distinctio at hic? Consectetur maiores error ad! Deserunt sit praesentium animi quo reprehenderit! Libero in rerum ullam consequuntur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi optio iste quasi expedita. Fuga est similique voluptatem, voluptas ex, labore veritatis, repudiandae eius molestiae, quas sint minima non. Hic, unde.</p>
@@ -68,7 +74,7 @@
                 </div>
             </div>
         </div>
-
+</div>
 
 <div class="about">
             <div class="container">
@@ -82,6 +88,9 @@
 
                          
                         </div>
+                         <div>
+                        	<img src="{{ URL::to('images/line.png') }}">
+                        </div>
                   
 
                         <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi iusto sequi reprehenderit soluta earum suscipit inventore! Eligendi eaque voluptas, quibusdam quasi id architecto reiciendis quas accusantium quaerat nesciunt veniam. Ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex reiciendis ratione dolores laudantium inventore recusandae voluptatum, vitae hic quos optio eius ea alias aspernatur officia maxime repellat laborum expedita at.</p>
@@ -93,6 +102,7 @@
                 </div>
             </div>
         </div>
+
 
 
 <div class="about">
@@ -112,6 +122,9 @@
                         
   
                         </div>
+                         <div>
+                        	<img src="{{ URL::to('images/line.png') }}">
+                        </div>
 
 
                         <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis voluptatum aliquam dignissimos placeat quas est ducimus debitis quaerat culpa quos sint, laboriosam provident nostrum? Animi dolorem quam culpa ad aliquam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio dicta, enim, architecto excepturi aperiam quo vitae accusantium ea incidunt nostrum harum autem voluptas ullam optio in ut ratione consectetur maiores.</p>
@@ -124,88 +137,120 @@
 
 	
           
-          
+          <div  class="jumbotron back_image2" style="padding-left:0px;margin-top: 100px; ">
+          	<div class="container mainText2 mx-auto">
 
-                <div class="row" style="padding-top: 30px">
-                	<div class="col text-center custom_head">
-                        <div class="custom_head">
-                            <h3 class="para3">Our Professional And Friendly Team </h3>
+             <div class="row">
+   					 <div class="col-sm text-center">
+   					 	<div class="fancy-background">
+   			 				<h1 id="para3">Meet Our Creative And Professional Team</h1>
+   			 				</div>
+   			 			</div>
+   	
+  			</div>
+             </div>
+         </div>
+
+         <div class="about">
+            <div class="container">
+                
+                <div class="row">
+                   
+                    <div class="col-md-4 col-sm-4">
+                        <img src="{{ URL::to('images/staff2.png') }}">
+                    </div>
+          
+                    <div class="col-md-8 col-sm-8 text-center ">
+                        <div class="custom-heading" >
+                            <h3 id="para1">We are always ready!</h3>
+                           
+
+                        
+  
                         </div>
 
+
+                       <figcaption>
+                                    <h4 class="staff-name">Kaith Lopez</h4>
+                                    <p>Head Chef </p>
+
+                                   
+
+                                    <p class="staff-about text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam, perferendis nobis, reiciendis quaerat debitis! Quas aspernatur recusandae reprehenderit. Aut omnis vitae dolores dignissimos quasi sapiente odio! Repellendus, vel, quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
+                                </figcaption>   
+
+                    </div>
                 </div>
-
-
             </div>
-        
-            <div class="container" style="padding-top: 30px">
-               
+        </div>
+
+         <div class="about">
+            <div class="container">
+                
                 <div class="row">
-                    <div class="col-md-4 col-sm-4">
-                        <div class="team-member">                                
-                            <figure>
-                                <img src="{{ URL::to('images/popo.png') }}" />
+              
+          
+                    <div class="col-md-8 col-sm-8 text-center ">
+                        <div class="custom-heading" >
+                            <h3 id="para1">We are fast!</h3>
+                           
 
-                                <figcaption>
-                                    <h4 class="member-name">Ann Rakky</h4>
-                                    <p class="member-position">Chef de fua</p>
+                        
+  
+                        </div>
 
-                                    <!-- .divider start -->
-                                    <div class="divider">
-                                        <span class="hr-simple"></span>
-                                    </div>
 
-                                    <p class="member-about text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
+
+                       <figcaption>
+                                    <h4 class="staff-name">Edward Smiths</h4>
+                                    <p >Sous Cheft</p>
+
+                                   
+
+                                    <p class="staff-about text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam, perferendis nobis, reiciendis quaerat debitis! Quas aspernatur recusandae reprehenderit. Aut omnis vitae dolores dignissimos quasi sapiente odio! Repellendus, vel, quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
                                 </figcaption>   
-                            </figure>
-                        </div><!-- .team-member end -->
 
-                    </div><!-- .col-md-4 end -->
+                    </div>
+
 
                     <div class="col-md-4 col-sm-4">
-                        <div class="team-member">                               
-                            <figure>
-                                <img src="{{ URL::to('images/popo.png') }}" />
+                        <img src="{{ URL::to('images/staff3.png') }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="about">
+            <div class="container">
+                
+                <div class="row">
+                   
+                    <div class="col-md-4 col-sm-4">
+                        <img src="{{ URL::to('images/staff4.png') }}">
+                    </div>
+          
+                    <div class="col-md-8 col-sm-8 text-center ">
+                        <div class="custom-heading" >
+                            <h3 id="para1">We make things yummy!</h3>
+                           
 
-                                <figcaption>
-                                    <h4 class="member-name">Kaith Simpson</h4>
-                                    <p class="member-position">Manager</p>
+                        
+  
+                        </div>
 
-                                    <!-- .divider start -->
-                                    <div class="divider">
-                                        <span class="hr-simple"></span>
-                                    </div>
 
-                                    <p class="member-about text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo modi aspernatur repellat inventore, ullam suscipit magni eveniet temporibus quis praesentium sequi obcaecati, doloremque adipisci! Molestias fugiat consequatur eos delectus animi!</p>
+                       <figcaption>
+                                    <h4 class="staff-name">Lola Rey</h4>
+                                    <p >Chef de Partie</p>
+
+                                    <p class="staff-about text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam, perferendis nobis, reiciendis quaerat debitis! Quas aspernatur recusandae reprehenderit. Aut omnis vitae dolores dignissimos quasi sapiente odio! Repellendus, vel, quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
                                 </figcaption>   
-                            </figure>
-                        </div><!-- .team-member end -->
-                    </div><!-- .col-md-4 end -->
 
-                    <div class="col-md-4 col-sm-4">
-                        <div class="team-member">                              
-                            <figure>
-                                <img src="{{ URL::to('images/popo.png') }}" />
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                                <figcaption>
-                                    <h4 class="member-name">Leyla Smiths</h4>
-                                    <p class="member-position">Head Waiter</p>
-
-                                    <!-- .divider start -->
-                                    <div class="divider">
-                                        <span class="hr-simple"></span>
-                                    </div>
-
-                                    <p class="member-about text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae delectus odit sunt quaerat officiis ex iste molestias sapiente, autem accusantium quia qui blanditiis minus harum consequuntur nulla unde doloremque labore.</p>
-                                </figcaption>    
-                            </figure>
-                        </div><!-- .team-member end -->
-                    </div><!-- .col-md-4 end -->
-                </div><!-- .row end -->
-            </div><!-- .container end -->
-        </div><!-- .page-content end -->
-
-
-
+        
 
 
 @endsection
