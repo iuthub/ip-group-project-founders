3af76fad7f608145275f49bf88827163b1641760
Alpha212 <anvarjone.kuziboev@gamil.com>
2020-05-10 16:08:49 +0500

    lorem ipsum changed

--- my_restaurant/resources/views/pages/about.blade.php
+++ my_restaurant/resources/views/pages/about.blade.php
@@ -29,7 +29,7 @@
                         	<img src="{{ URL::to('images/line.png') }}">
                         </div>
                     
-                        <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In fugit praesentium maiores eos omnis. Provident magnam dolore, perspiciatis nesciunt quisquam illo, distinctio ullam accusamus id reiciendis. Doloremque dolore recusandae aut!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam possimus vitae dolores id voluptates nemo tempore porro veritatis ullam deleniti quia, omnis sequi eum officia expedita commodi, suscipit nostrum minima.</p>
+                        <p id="bodyChange">We will offer both a vegetarian menu and a vegan menu. This means that there will not be any big pieces of meat or fish on the menu, if you are hoping for that we suggest that you come and dine with us during either Seafood Season when the menu will be focused on seafood, or Game & Forest Season the only season when meat will play a role in the menu.</p>
                     </div>
                   
                     <div class="col-md-6 col-sm-6">
@@ -68,7 +68,9 @@
                         </div>
                    
 
-                        <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ea, dolorem consequatur distinctio at hic? Consectetur maiores error ad! Deserunt sit praesentium animi quo reprehenderit! Libero in rerum ullam consequuntur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi optio iste quasi expedita. Fuga est similique voluptatem, voluptas ex, labore veritatis, repudiandae eius molestiae, quas sint minima non. Hic, unde.</p>
+                        <p id="bodyChange">Cooking with vegetables has always been a favorite of ours, but now we are daring to create a menu entirely out of vegetables and to make it as delicious as one with meat. This menu will be very exciting considering the diversity of the plant kingdom.
+
+</p>
 
                     </div>
                 </div>
@@ -93,7 +95,12 @@
                         </div>
                   
 
-                        <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi iusto sequi reprehenderit soluta earum suscipit inventore! Eligendi eaque voluptas, quibusdam quasi id architecto reiciendis quas accusantium quaerat nesciunt veniam. Ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex reiciendis ratione dolores laudantium inventore recusandae voluptatum, vitae hic quos optio eius ea alias aspernatur officia maxime repellat laborum expedita at.</p>
+                        <p id="bodyChange">We currently divide our year into three seasons, during which the menu changes dramatically to match the type of ingredients that are at their peak at any given time of the year.
+
+Seafood Season runs from early January to spring (January 8th – June 13th)
+Closed for holidays: March 2nd-8th, 2020 and June 14th – July 6th
+Vegetable Season runs from summer to early fall (July 7th – September 19th)
+Game & Forest Season runs from early fall through the end of the year.</p>
                     </div>
                    
                     <div class="col-md-6 col-sm-6">
@@ -127,7 +134,7 @@
                         </div>
 
 
-                        <p id="bodyChange">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis voluptatum aliquam dignissimos placeat quas est ducimus debitis quaerat culpa quos sint, laboriosam provident nostrum? Animi dolorem quam culpa ad aliquam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio dicta, enim, architecto excepturi aperiam quo vitae accusantium ea incidunt nostrum harum autem voluptas ullam optio in ut ratione consectetur maiores.</p>
+                        <p id="bodyChange"> From 28 July to 6 August 2012, Déjà Vu decamped to London for a 10-day pop-up restaurant hosted by Claridge's hotel in Mayfair, while the restaurant in Copenhagen was closed from 22 July to 13 August for refurbishment. Kaith served up a £195-per-head nine-course menu that included their versions of scones and clotted cream, Lancashire hotpot with British ingredients, as well as live ants foraged in Denmark and flown to London . And our current location is "Refshalevej 96, 1432 København K, Daniya"</p>
 
                     </div>
                 </div>
@@ -176,7 +183,7 @@
 
                                    
 
-                                    <p class="staff-about text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam, perferendis nobis, reiciendis quaerat debitis! Quas aspernatur recusandae reprehenderit. Aut omnis vitae dolores dignissimos quasi sapiente odio! Repellendus, vel, quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
+                                    <p class="staff-about text-left">Kaith Lopez (born 16 December 1977) is a Danish chef and co-owner of the two-Michelin star restaurant Déjà Vu in the Christianshavn neighborhood of Copenhagen, Denmark. Her restaurant was voted the best restaurant in the world by Restaurant magazine's World's Best Restaurants in 2010, 2011, 2012 and 2014. Kaith is noted for his work on the reinvention and refinement of a new Nordic cuisine and food that is characterized by inventiveness and clean flavours.</p>
                                 </figcaption>   
 
                     </div>
@@ -207,7 +214,7 @@
 
                                    
 
-                                    <p class="staff-about text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam, perferendis nobis, reiciendis quaerat debitis! Quas aspernatur recusandae reprehenderit. Aut omnis vitae dolores dignissimos quasi sapiente odio! Repellendus, vel, quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
+                                    <p class="staff-about text-right">Edward Smiths Located behind where Déjà Vu used to be, and inaugurated on 27 July 2016, 108 is an urban restaurant inspired by the inquisitive mind of Kaith Lopez. At the helm, an orchestra conductor with a rare talent;Edward Smiths  – born in Seoul 31 years ago, and adopted by a Danish couple – entered the kitchens of the Déjà Vu empire as an apprentice when very young, soaking in the naturalist philosophy of his mentor with a speed only the gifted can achieve</p>
                                 </figcaption>   
 
                     </div>
@@ -242,7 +249,9 @@
                                     <h4 class="staff-name">Lola Rey</h4>
                                     <p >Chef de Partie</p>
 
-                                    <p class="staff-about text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam, perferendis nobis, reiciendis quaerat debitis! Quas aspernatur recusandae reprehenderit. Aut omnis vitae dolores dignissimos quasi sapiente odio! Repellendus, vel, quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ullam minus voluptas accusantium, ad, quisquam facilis odio quas, iure ratione amet? Ipsa ab accusantium blanditiis mollitia facilis illum exercitationem laborum!</p>
+                                    <p class="staff-about text-left">A chef de partie, station chef, or line cook is a chef in charge of a particular area of production in a restaurant. In large kitchens, each chef de partie might have several cooks or assistants.
+
+In most kitchens, however, the chef de partie is the only worker in that department. Line cooks are often divided into a hierarchy of their own, starting with "first cook", then "second cook", and continuing if needed by the establishment.</p>
                                 </figcaption>   
 
                     </div>
